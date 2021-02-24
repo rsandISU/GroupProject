97f1fc86eb84e6ee6a8ae2449464dcc835533a9a
tergav17 <gctersteeg@gmail.com>
2021-02-23 21:34:33 -0600

    Changed rotation test to be more funny

--- src/test/SetterTest.java
+++ src/test/SetterTest.java
@@ -2,6 +2,7 @@ package test;
 
 import engine.Canvas;
 import engine.GameElement;
+import engine.Sprite;
 import engine.SpriteTransformable;
 import util.ResourceLoader;
 
@@ -10,18 +11,30 @@ public class SetterTest implements GameElement {
     private Canvas can;
 
     private SpriteTransformable spr;
+    private Sprite background;
+    private Sprite foreground;
+    private SpriteTransformable funny;
 
     public SetterTest(Canvas c) {
         this.can = c;
 
-        spr = new SpriteTransformable(ResourceLoader.getImage("funni.png"), 0, 0, 150, 300, 0);
-        spr.setOffsetX(0.5);
-        spr.setOffsetY(0.5);
+        spr = new SpriteTransformable(ResourceLoader.getImage("funni.png"), 0, 0, 150, 300, 5);
+
+        background = new Sprite(ResourceLoader.getImage("testBackground.png"), 0, 0, 2);
+        foreground = new Sprite(ResourceLoader.getImage("testForeground.png"), 0, 0, 0);
+        funny = new SpriteTransformable(ResourceLoader.getImage("funniActive.png"), 960 - 250, 50, 500, 1000, 1);
+
+        funny.setOffsetY(1);
     }
 
     @Override
     public void start() {
+        can.add(funny);
+        can.add(background);
+        can.add(foreground);
         can.add(spr);
+
+        funny.setAngle(180);
     }
 
     @Override
@@ -31,9 +44,10 @@ public class SetterTest implements GameElement {
 
     @Override
     public void update() {
-        spr.setAngle(spr.getAngle() + 1);
+        spr.setAngle(spr.getAngle() - 1);
+        funny.setAngle(funny.getAngle() + 0.3);
 
-        spr.setX(can.getMouseX() - 75);
-        spr.setY(can.getMouseY() - 150);
+        //spr.setX(can.getMouseX() - 75);
+        //spr.setY(can.getMouseY() - 150);
     }
 }
