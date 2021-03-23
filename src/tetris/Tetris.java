4032e1d3b3e553ecd7df69a07feaab638c7412ef
Eric Schmitt <ews1@iastate.edu>
2021-03-23 16:10:39 -0500

    added tetris background

--- src/tetris/Tetris.java
+++ src/tetris/Tetris.java
@@ -14,43 +14,42 @@ import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
 public class Tetris implements GameElement, MouseMotionListener, KeyListener {
-    Sprite piece, gridSpr, rectSpr;
+    Sprite piece, gridSpr, left, right, bottom;
     Canvas c;
 
 
     BufferedImage Grid = ResourceLoader.getImage("tetrisImages/blackGrid.jpg");
-    BufferedImage Rect = ResourceLoader.getImage("tetrisImages/blackRect.jpg");
     BufferedImage LPiece = ResourceLoader.getImage("tetrisImages/LPiece.png");
 
+    //Tetris background
+    BufferedImage LeftTB = ResourceLoader.getImage("tetrisImages/leftTetrisBackground.jpg");
+    BufferedImage RightTB = ResourceLoader.getImage("tetrisImages/rightTetrisBackground.jpg");
+    BufferedImage BottomTB = ResourceLoader.getImage("tetrisImages/bottomTetrisBackground.jpg");
 
 
     //Pacman vars
     int x = 0;
     int y = 0;
-    int width = 200;
-    int height = 200;
-    //new Rectangle r1 = (10, 20, width, height);
     int moveDist = 50;
     int drop = 0;
-    int move = 0;
-    boolean moveRight, moveLeft, moveDown;
 
-//    double x = 0;
-//    double y = 0;
 
-    double mx = 10;
-    double my = 0;
 
 
 
     public Tetris(Canvas c){
         this.c = c;
-        
+
         //each block should have a side length of moveDist
-        piece = new Sprite(LPiece, 0, 0, moveDist*3, moveDist*2, 1);
+        piece = new Sprite(LPiece, 0, 0, moveDist*3, moveDist*2, 10);
 
         //grid is 29 tall by 52 wide
         gridSpr = new Sprite(Grid, 0, 0, moveDist*52, moveDist*29, 0);
+
+        //background is 38.5 wide by 21.5 tall
+        left = new Sprite(LeftTB, 0, 0, moveDist*10, moveDist*22,1);
+        right = new Sprite(RightTB, moveDist*28, 0, moveDist*11, moveDist*22,2);
+        bottom = new Sprite(BottomTB, moveDist*10, moveDist*21, moveDist*18, moveDist*4,4);
 
 
     }
@@ -64,6 +63,9 @@ public class Tetris implements GameElement, MouseMotionListener, KeyListener {
         y = 100;
         c.put(piece, "LPiece");
         c.put(gridSpr, "Grid");
+        c.put(left, "LeftTB");
+        c.put(right, "RightTB");
+        c.put(bottom, "BottomTB");
 
     }
 
